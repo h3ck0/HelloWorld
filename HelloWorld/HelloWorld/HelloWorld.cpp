45d77a55a80670627e0bfc19f890e9ed8dbd75b4
Henning <Henning@OPPENHEIMER>
2017-01-09 22:21:29 +0100

    neur commit

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -14,14 +14,22 @@ int main(array<System::String ^> ^args)
 	Console::WriteLine(L" ////////////////////////////////////////////////////////////");
     Console::WriteLine(L" //                   Hello World                          //");
 	Console::WriteLine(L" ////////////////////////////////////////////////////////////");
-
+	
+	// for schleife maximal 5 mal
 	for (size_t i = 0; i < 5; i++)
 	{
 		Console::WriteLine("\n bitte eine Zahl eingeben:");
 		line = Console::ReadLine();
-		Console::WriteLine("\n OK du hast " + line + " eingegeben");
-		Console::ReadLine();
+		Console::WriteLine("\n ==> OK du hast " + line + " eingegeben <==");
+		line = Console::ReadLine();
 		Console::WriteLine(L" ////////////////////////////////////////////////////////////");
+
+		// schneller ausstieg
+		if (line == "13")
+		{
+			return 0;
+		}
+
 	}
 	
 	return 0;
